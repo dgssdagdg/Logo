4c7060ce1ea7e0d1e8cb008bd5d1a8a9b6b2714d
dgssdagdg <100527555+dgssdagdg@users.noreply.github.com>
2023-06-12 20:34:05 +0300

    add

---
+++ js/script.js
@@ -0,0 +1,28 @@
+document.addEventListener('click', function(e) {
+    let menuBtn = document.querySelector('.menu-btn');
+    let menu = document.querySelector('.menu');
+    let body =  document.body;
+    if(e.target.closest('.menu-btn')) {
+        menuBtn.classList.toggle('active');
+        menu.classList.toggle('active');
+        body.classList.toggle('body-overflow');
+    } else if (menu.closest('.active') && !e.target.closest('.menu') && menuBtn.closest('.active')) {
+        menuBtn.classList.remove('active');
+        menu.classList.remove('active');
+        body.classList.remove('body-overflow');
+    }
+
+    if (e.target.closest('.menu-list-top')) {
+      let list = e.target.closest('.menu-list-item')
+      list.classList.toggle('menu-list-item-active')
+    }
+
+})
+
+window.onscroll = function(){
+  var html = document.documentElement, body = document.body;
+  var BlkStyle = document.querySelector('.header');
+  if(html.scrollTop>50||body.scrollTop>50) {
+      BlkStyle.classList.add('header-flex')
+  } else BlkStyle.classList.remove('header-flex');
+}
